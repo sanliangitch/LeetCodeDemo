ce75ad61a076dfbd01a38b8819be304acdb2ccad
wulang969696 <sanliangitch96@gmail.com>
2019-09-12 22:14:58 +0800

    no message

--- src/main/java/cn/wulang/LeetCodeDemo/AddTwoNumbers.java
+++ src/main/java/cn/wulang/LeetCodeDemo/AddTwoNumbers.java
@@ -16,6 +16,10 @@ package cn.wulang.LeetCodeDemo;
  * @create 2019/9/12/15:10
  */
 public class AddTwoNumbers {
+    /**
+     * 小技巧：对于链表问题，返回结果为头结点时，通常需要先初始化一个预先指针 pre，该指针的下一个节点指向真正的头结点head。
+     * 使用预先指针的目的在于链表初始化时无可用节点值，而且链表构造过程需要指针移动，进而会导致头指针丢失，无法返回结果。
+     */
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummyHead = new ListNode(0);
         ListNode p = l1,q = l2,curr = dummyHead;
